6fb2245a473f5312fcfed393032bc0550f4369f9
Ollebogge <obogardh@gmail.com>
2024-12-06 16:57:44 +0100

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,37 @@
+const checkboxInput = document.querySelector('input[type="checkbox"]');
+
+const textFields = document.querySelectorAll('input[type="text"]');
+
+const submitButton = document.querySelector('button');
+
+const emptyDiv = document.querySelector('div');
+
+const colorInput = document.querySelector('#color');
+
+
+//funktion för att hantera de event som sker
+function handleInputEvent(e) {
+    console.log("Event triggat av: ", e.target);//Skriver ut elementet som triggat eventet
+    //Kontrollerar om det är inputfältet med id "content"
+    if (e.target.id === "content") {
+        const contentValue = e.target.value;//Hämta värdet från inputfältet jag har
+        emptyDiv.textContent = contentValue;//Sätter värdet i div elementet
+    }
+}
+//eventlyssnare för textfältet
+textFields.forEach((textField) => {
+    textField.addEventListener("input", handleInputEvent);
+});
+//eventlyssnaren för checkboxen
+checkboxInput.addEventListener("change", () => {
+    if (checkboxInput.checked) {
+        emptyDiv.style.backgroundColor = colorInput.value || "lightgray"; // Använd färgen från inputfältet
+    } else {
+        emptyDiv.style.backgroundColor = "transparent"; // Återställ till transparent
+    }
+});
+
+//eventlyssnaren för knappen
+submitButton.addEventListener("click", () => {
+    emptyDiv.style.display = "home"; //gömmer div elementet
+});
